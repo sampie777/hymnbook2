cbf9583c4dfe80bc1c4899f85057e8eff60157f0
S. Jansen <samuelanton@hotmail.com>
2021-10-16 12:19:58 +0200

    refactor(package): Renamed package name from com.hymnbook2 to nl.sajansen.hymnbook2

--- android/app/src/main/java/nl/sajansen/hymnbook2/MainApplication.java
+++ android/app/src/main/java/nl/sajansen/hymnbook2/MainApplication.java
@@ -1,4 +1,4 @@
-package com.hymnbook2;
+package nl.sajansen.hymnbook2;
 
 import android.app.Application;
 import android.content.Context;
@@ -68,7 +68,7 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.hymnbook2.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("nl.sajansen.hymnbook2.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
