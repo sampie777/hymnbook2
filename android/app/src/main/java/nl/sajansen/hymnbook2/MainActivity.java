c41e9096bd9dc28c8f77c2475a10e4c917a800ec
S. Jansen <samuelanton@hotmail.com>
2021-12-11 12:11:31 +0200

    fix(screen): Screen be put in split mode without crashing.

--- android/app/src/main/java/nl/sajansen/hymnbook2/MainActivity.java
+++ android/app/src/main/java/nl/sajansen/hymnbook2/MainActivity.java
@@ -1,6 +1,7 @@
 package nl.sajansen.hymnbook2;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +12,10 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "hymnbook2";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
