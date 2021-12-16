f2e578258e82c047154df8c5da1526240f79cb50
S. Jansen <sa.jansen1@gmail.com>
2021-12-16 15:12:53 +0200

    feat(release): Added command for patch release.

--- release.sh
+++ release.sh
@@ -1,35 +1,93 @@
 #!/bin/bash
 
-yarn test || exit 1
+progname=$(basename $0)
 
-# Release version
-git checkout master || exit 1
-git merge develop || exit 1
+function usage {
+  cat << HEREDOC
 
-npm --no-git-tag-version version patch || exit 1
+     Usage: $progname [command]
 
-yarn test || exit 1
+     commands:
+       patch                Release a patch version (0.0.X)
+       minor                Release a minor version (0.X.0)
+       -h, --help           Show this help message and exit
 
-RELEASE_VERSION=$(sed 's/.*"version": "\(.*\)".*/\1/;t;d' ./package.json)
-echo "Release version: ${RELEASE_VERSION}"
+HEREDOC
+}
 
-git add package.json || exit 1
-git commit -m "version release" || exit 1
-git tag "v${RELEASE_VERSION}" || exit 1
-git push -u origin master --tags || exit 1
+function releasePatch {
+  yarn test || exit 1
 
-yarn bundle || exit 1
-yarn build || exit 1
+  git checkout master || exit 1
 
-# Generate next development version
-git checkout develop || exit 1
-git rebase master || exit 1
+  # Create patch versionn
+  CURRENT_VERSION=$(sed 's/.*"version": "\(.*\)".*/\1/;t;d' ./package.json)
+  RELEASE_VERSION=$(echo ${CURRENT_VERSION} | awk -F'.' '{print $1"."$2"."$3+1}')
 
-npm --no-git-tag-version version minor || exit 1
-RELEASE_VERSION=$(sed 's/.*"version": "\(.*\)".*/\1/;t;d' ./package.json)-SNAPSHOT
-echo "Next development version: ${DEV_VERSION}"
-npm --no-git-tag-version version ${RELEASE_VERSION} || exit 1
+  git merge develop || exit 1
 
-git add package.json || exit 1
-git commit -m "next development version" || exit 1
-git push -u origin develop --tags || exit 1
+  npm --no-git-tag-version version "${RELEASE_VERSION}" || exit 1
+
+  pushAndRelease
+}
+
+function releaseMinor {
+  yarn test || exit 1
+
+  git checkout master || exit 1
+  git merge develop || exit 1
+
+  npm --no-git-tag-version version patch || exit 1
+
+  pushAndRelease
+}
+
+function pushAndRelease {
+  yarn test || exit 1
+
+  RELEASE_VERSION=$(sed 's/.*"version": "\(.*\)".*/\1/;t;d' ./package.json)
+  echo "Release version: ${RELEASE_VERSION}"
+
+  git add package.json || exit 1
+  git commit -m "version release" || exit 1
+  git tag "v${RELEASE_VERSION}" || exit 1
+  git push -u origin master --tags || exit 1
+
+  yarn bundle || exit 1
+  yarn build || exit 1
+}
+
+function setNextDevelopmentVersion {
+  git checkout develop || exit 1
+  git rebase master || exit 1
+
+  # Generate next (minor) development version
+  npm --no-git-tag-version version minor || exit 1
+  DEV_VERSION=$(sed 's/.*"version": "\(.*\)".*/\1/;t;d' ./package.json)-SNAPSHOT
+
+  echo "Next development version: ${DEV_VERSION}"
+  npm --no-git-tag-version version ${DEV_VERSION} || exit 1
+
+  git add package.json || exit 1
+  git commit -m "next development version" || exit 1
+  git push -u origin develop --tags || exit 1
+}
+
+command="$1"
+case $command in
+  patch)
+    releasePatch
+    setNextDevelopmentVersion
+    ;;
+  minor)
+    releaseMinor
+    setNextDevelopmentVersion
+    ;;
+  -h|--help)
+    usage
+    ;;
+  *)
+    echo "Invalid command"
+    exit 1
+    ;;
+esac
