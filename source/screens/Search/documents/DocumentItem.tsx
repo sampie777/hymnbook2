b5a732c249204fd029b8379e6aa56c432fe5bbe0
S. Jansen <samuelanton@hotmail.com>
2022-01-20 08:50:37 +0200

    feat(documents): Show parent group for documents when searching.

--- source/screens/Search/documents/DocumentItem.tsx
+++ source/screens/Search/documents/DocumentItem.tsx
@@ -5,33 +5,59 @@ import { Text, TouchableOpacity, StyleSheet } from "react-native";
 
 
 interface ScreenProps {
-  document: Document
-  onPress?: (document: Document) => void,
+  document: Document;
+  onPress?: (document: Document) => void;
+  searchText?: string;
 }
 
-const DocumentItem: React.FC<ScreenProps> = ({ document, onPress }) => {
+const DocumentItem: React.FC<ScreenProps> = ({ document, onPress, searchText }) => {
   const styles = createStyles(useTheme());
 
-  return (<TouchableOpacity onPress={() => onPress?.(document)} style={styles.searchListItem}>
-    <Text style={styles.searchListItemText}>{document.name}</Text>
+  return (<TouchableOpacity onPress={() => onPress?.(document)}
+                            style={styles.container}>
+    {searchText === undefined || searchText.length === 0 ? undefined :
+      <Text style={styles.parentName}>
+        {Document.getParent(document)?.name}
+      </Text>
+    }
+    <Text style={[
+      styles.itemName,
+      (!(searchText === undefined || searchText.length === 0) ? {} : styles.itemExtraPadding)
+    ]}>
+      {document.name}
+    </Text>
   </TouchableOpacity>);
 };
 
 export default DocumentItem;
 
 const createStyles = ({ colors }: ThemeContextProps) => StyleSheet.create({
-  searchListItem: {
+  container: {
     marginBottom: 1,
     backgroundColor: colors.surface1,
     borderColor: colors.border,
     borderBottomWidth: 1,
-    flexDirection: "row",
-    alignItems: "center"
+    flexDirection: "column",
+    alignItems: "flex-start"
   },
-  searchListItemText: {
-    padding: 15,
+
+  parentName: {
+    paddingTop: 8,
+    paddingHorizontal: 15,
+    fontSize: 14,
+    color: colors.textLighter,
+    fontFamily: "sans-serif-light",
+    fontStyle: "italic"
+  },
+
+  itemName: {
+    paddingBottom: 15,
+    paddingHorizontal: 15,
     fontSize: 20,
     flex: 1,
     color: colors.text
   },
+  itemExtraPadding: {
+    padding: 15
+  }
 });
