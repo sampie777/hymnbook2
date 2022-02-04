db2e89f7d2519c29ac7149695860faebbd822d11
S. Jansen <sa.jansen1@gmail.com>
2022-02-04 10:20:42 +0200

    feat(notes): Added supportive lines for notes out of reach feat(notes): Created config for variables.

--- source/components/melody/voiceItems/VoiceItemIntroElement.tsx
+++ source/components/melody/voiceItems/VoiceItemIntroElement.tsx
@@ -2,20 +2,20 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 import Svg, { G } from "react-native-svg";
 import Lines from "./Lines";
+import { AbcConfig } from "./config";
 
 interface Props {
-  verticalSpacing: number;
+  scale: number;
 }
 
-const VoiceItemIntroElement: React.FC<Props> = ({ verticalSpacing }) => {
-  const padding = verticalSpacing / 10 * 40;
-  const width = padding + 5 + 6;
-  const height = 5 * verticalSpacing;
+const VoiceItemIntroElement: React.FC<Props> = ({ scale }) => {
+  const width = 40;
+  const height = AbcConfig.topSpacing + 5 * AbcConfig.lineSpacing + AbcConfig.bottomSpacing;
 
   return <View style={styles.container}>
     <Svg width={width} height={height} viewBox={`0 0 ${width} ${height}`}>
-      <G scale={1} y={2}>
-        <Lines verticalSpacing={verticalSpacing} />
+      <G scale={scale} y={AbcConfig.topSpacing}>
+        <Lines />
       </G>
     </Svg>
   </View>;
