8484a36202ca3add4b855917ba72f0c96ecc626c
S. Jansen <sa.jansen1@gmail.com>
2022-02-09 17:40:55 +0200

    fix(notes): Improved line generation performance.

--- source/components/melody/other/Background.tsx
+++ source/components/melody/other/Background.tsx
@@ -4,36 +4,34 @@ import { AbcConfig } from "../voiceItems/config";
 import Svg, { G } from "react-native-svg";
 import Lines from "../voiceItems/Lines";
 
-const Line: React.FC<{ scale: number, width: number }>
-  = ({ scale, width }) => {
-  return <Svg width={width * scale} height={AbcConfig.totalLineHeight * scale}
-              viewBox={`0 0 ${width * scale} ${AbcConfig.totalLineHeight * scale}`}>
-    <G scale={scale} y={AbcConfig.topSpacing * scale}>
-      <Lines width={width} />
-    </G>
-  </Svg>;
-};
-
 interface Props {
   scale: number;
 }
 
 const Background: React.FC<Props> = ({ scale }) => {
   const [screenWidth, setScreenWidth] = useState(Dimensions.get("window").width);
+  const [screenHeight, setScreenHeight] = useState(Dimensions.get("window").height);
   const [lineCount, setLineCount] = useState(1);
-
 
   const onLayout = (e: LayoutChangeEvent) => {
     const { width, height } = e.nativeEvent.layout;
-    const newLineCount = Math.round(height / AbcConfig.totalLineHeight);
+    const newLineCount = Math.ceil(height / AbcConfig.totalLineHeight);
 
     setScreenWidth(width);
+    setScreenHeight(height);
     setLineCount(newLineCount);
   };
 
   return <View style={styles.container} onLayout={onLayout}>
-    {Array.from({ length: lineCount }, (_, i) =>
-      <Line key={i} scale={scale} width={screenWidth} />)}
+    <Svg width={screenWidth} height={screenHeight}
+         viewBox={`0 0 ${screenWidth} ${screenHeight}`}>
+
+      {Array.from({ length: lineCount }, (_, i) =>
+        <G key={i} y={(i * AbcConfig.totalLineHeight + AbcConfig.topSpacing) * scale}>
+          <Lines width={screenWidth} scale={scale} />
+        </G>
+      )}
+    </Svg>
   </View>;
 };
 
@@ -41,7 +39,7 @@ const styles = StyleSheet.create({
   container: {
     position: "absolute",
     height: "100%",
-    width: "100%"
+    width: "100%",
   }
 });
 
--- source/components/melody/voiceItems/Lines.tsx
+++ source/components/melody/voiceItems/Lines.tsx
@@ -3,17 +3,18 @@ import { Line } from "react-native-svg";
 import { AbcConfig } from "./config";
 
 interface Props {
-  width: number
+  width?: number;
+  scale?: number;
 }
 
-const Lines: React.FC<Props> = ({width}) => {
+const Lines: React.FC<Props> = ({ width = 1000, scale = 1 }) => {
   return <>
     {[0, 1, 2, 3, 4].map(it =>
       <Line key={it}
-            x1={0} y1={it * AbcConfig.lineSpacing}
-            x2={width} y2={it * AbcConfig.lineSpacing}
+            x1={0} y1={it * AbcConfig.lineSpacing * scale}
+            x2={width} y2={it * AbcConfig.lineSpacing * scale}
             stroke="#333"
-            strokeWidth={AbcConfig.lineWidth} />
+            strokeWidth={AbcConfig.lineWidth * scale} />
     )}
   </>;
 };
