8aef0f51c24c7cd80641a3a36a073fbaa2cbee15
S. Jansen <samuelanton@hotmail.com>
2022-04-27 13:38:20 +0200

    fix(tests): Added new config values to tests

--- __tests__/scripts/survey.js
+++ __tests__/scripts/survey.js
@@ -1,56 +1,57 @@
 import Settings from "../../source/settings";
 import { Survey } from "../../source/scripts/survey";
+import config from "../../source/config";
 
 describe("test survey", () => {
-  it("needs to show when multiple of 4 but greater than 4", () => {
+  it("needs to show when multiple of displayInterval and greater or equal to minimumOpenedTimes", () => {
     Settings.surveyCompleted = false;
-    Settings.appOpenedTimes = 8;
+    Settings.appOpenedTimes = config.surveyMinimumAppOpenedTimes + config.surveyDisplayInterval;
 
     expect(Survey.needToShow()).toBe(true);
   });
   it("needs to show when multiple of 4 but greater than 4 (2)", () => {
     Settings.surveyCompleted = false;
-    Settings.appOpenedTimes = 12;
+    Settings.appOpenedTimes = config.surveyMinimumAppOpenedTimes + 3 * config.surveyDisplayInterval;
 
     expect(Survey.needToShow()).toBe(true);
   });
-  it("needs not to show when less than 5", () => {
+  it("needs not to show when less than minimumOpenedTimes", () => {
     Settings.surveyCompleted = false;
     Settings.appOpenedTimes = 0;
 
     expect(Survey.needToShow()).toBe(false);
   });
-  it("needs not to show when less than 5 (2)", () => {
+  it("needs not to show when less than minimumOpenedTimes (2)", () => {
     Settings.surveyCompleted = false;
     Settings.appOpenedTimes = 1;
 
     expect(Survey.needToShow()).toBe(false);
   });
-  it("needs not to show when less than 5", () => {
+  it("needs not to show when less than minimumOpenedTimes", () => {
     Settings.surveyCompleted = false;
-    Settings.appOpenedTimes = 4;
+    Settings.appOpenedTimes = config.surveyMinimumAppOpenedTimes - 1;
 
     expect(Survey.needToShow()).toBe(false);
   });
-  it("needs not to show when not multiple of 4", () => {
+  it("needs not to show when not multiple of displayInterval", () => {
     Settings.surveyCompleted = false;
-    Settings.appOpenedTimes = 5;
+    Settings.appOpenedTimes = config.surveyMinimumAppOpenedTimes + 1.5 * config.surveyDisplayInterval;
 
     expect(Survey.needToShow()).toBe(false);
   });
   it("needs not to show when already completed", () => {
     Settings.surveyCompleted = true;
-    Settings.appOpenedTimes = 8;
+    Settings.appOpenedTimes = config.surveyMinimumAppOpenedTimes + config.surveyDisplayInterval;
 
     expect(Survey.needToShow()).toBe(false);
   });
-  it("needs not to show when already completed or less than 5", () => {
+  it("needs not to show when already completed or less than minimumOpenedTimes", () => {
     Settings.surveyCompleted = true;
-    Settings.appOpenedTimes = 4;
+    Settings.appOpenedTimes = config.surveyMinimumAppOpenedTimes - 1;
 
     expect(Survey.needToShow()).toBe(false);
   });
-  it("needs not to show when already completed or less than 5", () => {
+  it("needs not to show when already completed or less than minimumOpenedTimes", () => {
     Settings.surveyCompleted = true;
     Settings.appOpenedTimes = 0;
 
