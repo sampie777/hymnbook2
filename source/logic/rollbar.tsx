c8b3275fd17ef67a7b39e4ae61426889533d17cb
S. Jansen <samuelanton@hotmail.com>
2022-11-07 11:28:32 +0200

    fix(rollbar): Require person ID for rollbar error reporting.

--- source/logic/rollbar.tsx
+++ source/logic/rollbar.tsx
@@ -4,25 +4,27 @@ import { getUniqueId, getVersion } from "react-native-device-info";
 import Config from "react-native-config";
 import config from "../config";
 
+const uniqueId = getUniqueId();
+
 const configuration = new Configuration(
   Config.ROLLBAR_API_KEY,
   {
     captureUncaught: true,
     captureUnhandledRejections: true,
-    enabled: !config.debugEmulators.includes(getUniqueId()),
+    enabled: !config.debugEmulators.includes(uniqueId) && uniqueId != null && uniqueId.length > 0,
     verbose: true,
     payload: {
       environment: process.env.NODE_ENV,
       client: {
         javascript: {
           source_map_enabled: true,
-          code_version: getVersion() + "." + Platform.OS,
+          code_version: getVersion() + "." + Platform.OS
         }
       },
       person: {
-        id: getUniqueId(),
+        id: uniqueId
       }
-    },
+    }
   });
 
 export const rollbar = new Client(configuration);
