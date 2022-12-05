121c8d055cc3fd8408929846c37b7ee9b8bdf9c6
S. Jansen <sa.jansen1@gmail.com>
2022-12-05 11:55:14 +0200

    fix: Fixed getItem error for flatlist

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
+    ["@babel/plugin-transform-flow-strip-types", { loose: true }],
     ["@babel/plugin-proposal-class-properties", { loose: true }],
     ["@babel/plugin-proposal-private-methods", { loose: true }],
     "@babel/plugin-proposal-unicode-property-regex",
