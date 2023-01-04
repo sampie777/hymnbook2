d20ffbc8c358ffd98e9c78abd3597d600b57ced0
S. Jansen <samuelanton@hotmail.com>
2023-01-04 12:44:32 +0200

    feat(log): Upload settings data if allowed.

---
+++ __tests__/logic/analytics.js
@@ -0,0 +1,21 @@
+import { rollbar } from "../../source/logic/rollbar";
+import { Analytics } from "../../source/logic/analytics";
+import settings from "../../source/settings";
+
+describe("test analytics", () => {
+  beforeEach(() => {
+    rollbar.debug.mockClear();
+  });
+
+  it("don't upload data when settings is disabled", () => {
+    settings.shareUsageData = false;
+    Analytics.uploadSettings();
+    expect(rollbar.debug.mock.calls.length).toBe(0);
+  });
+
+  it("upload data when settings is enabled", () => {
+    settings.shareUsageData = true;
+    Analytics.uploadSettings();
+    expect(rollbar.debug.mock.calls.length).toBe(1);
+  });
+});
