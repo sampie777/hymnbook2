5ae2b4a09a87559e5b6be8c317dd00a43dadc60e
S. Jansen <sa.jansen1@gmail.com>
2023-02-02 09:46:27 +0200

    feat(search): Included verse search. feat(search0: Implemented point search system.

--- source/logic/songs/songSearch.ts
+++ source/logic/songs/songSearch.ts
@@ -1,38 +1,88 @@
-import { Song } from "../db/models/Songs";
 import Db from "../db/db";
+import { Song } from "../db/models/Songs";
 import { SongSchema } from "../db/models/SongsSchema";
-import config from "../../config";
 
 export namespace SongSearch {
+  const titleMatchPoints = 2;
+  const verseMatchPoints = 1;
+
   export interface SearchResult {
     song: Song;
+    points: number;
   }
 
   export const find = (text: string, searchInTitles: boolean, searchInVerses: boolean): SearchResult[] => {
-    let query = "";
+    const results: SearchResult[] = [];
+
     if (searchInTitles) {
-      query += `name LIKE "*${text}*"`;
+      findByTitle(text).forEach(it => {
+        results.push({
+          song: it,
+          points: calculateMatchPointsForTitleMatch(it, text)
+        });
+      });
     }
+
+    // Add verse results to results
     if (searchInVerses) {
-      // todo
+      findByVerse(text).forEach(it => {
+        const existingResult = results.find(result => result.song.id === it.id);
+
+        const points = calculateMatchPointsForVerseMatch(it, text);
+
+        if (existingResult != null) {
+          existingResult.points += points;
+        } else {
+          results.push({
+            song: it,
+            points: points
+          });
+        }
+      });
     }
 
-    query = query.trim()
+    return results;
+  };
 
-    if (query.length === 0) {
-      return [];
-    }
-
-    query += ` LIMIT(${config.maxSearchResultsLength})`;
+  export const findByTitle = (text: string): Song[] => {
+    const query = `name LIKE[c] "*${text}*"`;
 
     const results = Db.songs.realm().objects<Song>(SongSchema.name)
       .sorted("name")
+      .sorted("number")
       .filtered(query);
 
-    return results.map(it => {
-      return {
-        song: it
-      };
+    return Array.from(results);
+  };
+
+  export const findByVerse = (text: string): Song[] => {
+    const query = `verses.content LIKE[c] "*${text}*"`;
+
+    const results = Db.songs.realm().objects<Song>(SongSchema.name)
+      .sorted("name")
+      .sorted("number")
+      .filtered(query);
+
+    return Array.from(results);
+  };
+
+  export const calculateMatchPointsForTitleMatch = (song: Song, text: string): number => {
+    return titleMatchPoints;
+  };
+
+  export const calculateMatchPointsForVerseMatch = (song: Song, text: string): number => {
+    let result = 0;
+
+    song.verses.forEach(it => {
+      const matches = it.content.match(new RegExp(`${text}`, "gi"));
+      if (matches == null) return;
+      result += matches.length * verseMatchPoints;
     });
+
+    const totalVerseLines: number = song.verses
+      .reduce((result, it) =>
+        result + it.content.split("\n").length, 0);
+
+    return result / totalVerseLines;
   };
 }
