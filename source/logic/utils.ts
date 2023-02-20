527f1614ad9ae888e78f11951d574c673d3d0a57
S. Jansen <samuelanton@hotmail.com>
2023-02-20 12:46:24 +0200

    fix(tests): Fixed import loop.

--- source/logic/utils.ts
+++ source/logic/utils.ts
@@ -137,8 +137,4 @@ export const runAsync = (f: () => any) => setTimeout(f, 0);
 export const emptyPromise = (): Promise<null> => new Promise((resolve => resolve(null)));
 export const emptyPromiseWithValue = <T>(value: T): Promise<T> => new Promise((resolve => resolve(value)));
 
-export class InterruptedError extends Error {
-  name = "InterruptedError";
-}
-
 export const isMelodyEnabled = () => Settings.showMelody || Platform.OS === "android";
