e0955ac0622bb1b8dcfe6b182ae30fcb8d596fab
S. Jansen <samuelanton@hotmail.com>
2023-02-27 14:30:43 +0200

    feat(api): Include HTTP response in error message. Also increased auth retry to 2 times.

--- source/logic/apiUtils.ts
+++ source/logic/apiUtils.ts
@@ -2,6 +2,12 @@ import { rollbar } from "./rollbar";
 
 export class HttpError extends Error {
   name = "HttpError"
+  response?: Response;
+
+  constructor(message?: string, response?: Response) {
+    super(message);
+    this.response = response;
+  }
 }
 
 export const throwErrorsIfNotOk = (response: Response) => {
@@ -12,16 +18,18 @@ export const throwErrorsIfNotOk = (response: Response) => {
   rollbar.error(`API request to '${response.url}' failed: (${response.status}) ${response.statusText}`);
   switch (response.status) {
     case 404:
-      throw new HttpError(`Could not find the requested data: (${response.status}) ${response.statusText}`);
+      throw new HttpError(`Could not find the requested data: (${response.status}) ${response.statusText}`, response);
     case 401:
-      throw new HttpError(`Could not retrieve the requested data: (${response.status}) Not authorized. \n\nGo to (advanced) settings and try to reset your authentication.`);
+      throw new HttpError(`Could not retrieve the requested data: (${response.status}) Not authorized. \n\nGo to (advanced) settings and try to reset your authentication.`, response);
     case 403:
-      throw new HttpError(`Could not retrieve the requested data: (${response.status}) Not authorized. \n\nGo to (advanced) settings and try to reset your authentication.`);
+      throw new HttpError(`Could not retrieve the requested data: (${response.status}) Not authorized. \n\nGo to (advanced) settings and try to reset your authentication.`, response);
+    case 410:
+      throw new HttpError(`Too many request (${response.status}).\n\nTake a break and try again later.`, response);
     case 429:
-      throw new HttpError(`Too many request (${response.status}).\n\nTake a break and try again later.`);
+      throw new HttpError(`Too many request (${response.status}).\n\nTake a break and try again later.`, response);
     case 500:
-      throw new HttpError(`Could not connect to server: (${response.status}) Internal server error`);
+      throw new HttpError(`Could not connect to server: (${response.status}) Internal server error`, response);
     default:
-      throw new HttpError(`Request failed: (${response.status}) ${response.statusText}`);
+      throw new HttpError(`Request failed: (${response.status}) ${response.statusText}`, response);
   }
 };
