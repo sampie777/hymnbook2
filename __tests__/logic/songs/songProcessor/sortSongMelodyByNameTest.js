7c666d515c1215230f1e26705bafa10c249a70d5
S. Jansen <samuelanton@hotmail.com>
2023-03-27 10:24:26 +0200

    fix(melodies): Sort melodies by name meaning instead of just alphabetic.

---
+++ __tests__/logic/songs/songProcessor/sortSongMelodyByNameTest.js
@@ -0,0 +1,28 @@
+import { SongProcessor } from "../../../../source/logic/songs/songProcessor";
+
+describe("sorting melodies by name", () => {
+  it("keeps Afrikaans language in mind", () => {
+    expect(SongProcessor.sortSongMelodyByName({ name: "Default" }, { name: "Default" })).toBe(-1);
+    expect(SongProcessor.sortSongMelodyByName({ name: "Default" }, { name: "Eerste melodie" })).toBe(-1);
+    expect(SongProcessor.sortSongMelodyByName({ name: "Eerste melodie" }, { name: "Default" })).toBe(1);
+    expect(SongProcessor.sortSongMelodyByName({ name: "Eerste melodie" }, { name: "Derde melodie" })).toBe(-1);
+    expect(SongProcessor.sortSongMelodyByName({ name: "Derde melodie" }, { name: "Eerste melodie" })).toBe(1);
+    expect(SongProcessor.sortSongMelodyByName({ name: "1" }, { name: "2" })).toBe(-1);
+    expect(SongProcessor.sortSongMelodyByName({ name: "2" }, { name: "1" })).toBe(1);
+    expect(SongProcessor.sortSongMelodyByName({ name: "1" }, { name: "1" })).toBe(0);
+  });
+  it("keeps English language in mind", () => {
+    expect(SongProcessor.sortSongMelodyByName({ name: "Default" }, { name: "Default" })).toBe(-1);
+    expect(SongProcessor.sortSongMelodyByName({ name: "Default" }, { name: "First melody" })).toBe(-1);
+    expect(SongProcessor.sortSongMelodyByName({ name: "First melody" }, { name: "Default" })).toBe(1);
+    expect(SongProcessor.sortSongMelodyByName({ name: "First melody" }, { name: "Third melody" })).toBe(-1);
+    expect(SongProcessor.sortSongMelodyByName({ name: "Third melody" }, { name: "First melody" })).toBe(1);
+  });
+  it("keeps Dutch language in mind", () => {
+    expect(SongProcessor.sortSongMelodyByName({ name: "Default" }, { name: "Default" })).toBe(-1);
+    expect(SongProcessor.sortSongMelodyByName({ name: "Default" }, { name: "Eerste melodie" })).toBe(-1);
+    expect(SongProcessor.sortSongMelodyByName({ name: "Eerste melodie" }, { name: "Default" })).toBe(1);
+    expect(SongProcessor.sortSongMelodyByName({ name: "Eerste melodie" }, { name: "Derde melodie" })).toBe(-1);
+    expect(SongProcessor.sortSongMelodyByName({ name: "Vijfde melodie" }, { name: "Eerste melodie" })).toBe(1);
+  });
+});
