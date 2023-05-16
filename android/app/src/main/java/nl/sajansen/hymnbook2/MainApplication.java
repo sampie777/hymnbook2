2b86d3850cdeedf57327b2aa5c249ece34a66dc1
S. Jansen <samuelanton@hotmail.com>
2023-05-16 10:54:47 +0200

    fix(android): Fix how rollbar config should determine the environment (NODE_ENV is not available).

--- android/app/src/main/java/nl/sajansen/hymnbook2/MainApplication.java
+++ android/app/src/main/java/nl/sajansen/hymnbook2/MainApplication.java
@@ -57,7 +57,7 @@ public class MainApplication extends Application implements ReactApplication {
     ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-    RollbarReactNative.init(this, BuildConfig.ROLLBAR_API_KEY, project.env.get("NODE_ENV");
+    RollbarReactNative.init(this, BuildConfig.ROLLBAR_API_KEY, BuildConfig.DEBUG ? "development" : "production");
   }
 
   /**
