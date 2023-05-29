c6690ba0d1b282b3e4f1f5537085b4640d1d08a5
S. Jansen <sa.jansen1@gmail.com>
2023-05-29 12:33:17 +0200

    refactor(settings): Split settings components into separate files.

---
+++ source/gui/screens/settings/components/SettingSwitchComponent.tsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { StyleSheet, Switch, Text, View } from "react-native";
+import { ThemeContextProps, useTheme } from "../../../components/ThemeProvider";
+import Settings from "../../../../settings";
+import { BaseSettingProps, createStyles as settingComponentCreateStyles } from "./SettingComponent";
+
+interface BooleanSettingProps extends BaseSettingProps<boolean> {
+  onPress?: (setValue: (newValue: boolean) => void, key: string | undefined, newValue: boolean) => void;
+}
+
+const SettingSwitchComponent: React.FC<BooleanSettingProps> =  ({
+                                         title,
+                                         description,
+                                         keyName,
+                                         value,
+                                         onPress = undefined,
+                                         isVisible = true,
+                                         lessObviousStyling = false
+                                       }) => {
+  if (!isVisible) {
+    return null;
+  }
+
+  const baseStyles = settingComponentCreateStyles(useTheme());
+  const styles = createStyles(useTheme());
+
+  if (value === undefined && keyName !== undefined) {
+    value = Settings.get(keyName);
+  }
+
+  if (onPress === undefined) {
+    onPress = (setValue, key, newValue) => setValue(newValue);
+  }
+
+  const [_value, _setValue] = useState(value);
+  const setValue = (newValue: boolean) => {
+    if (keyName !== undefined) {
+      // @ts-ignore
+      Settings[keyName] = newValue;
+      // @ts-ignore
+      _setValue(Settings[keyName]);
+    } else {
+      _setValue(newValue);
+    }
+  };
+
+  if (typeof _value !== "boolean") {
+    console.warn("Using boolean switch component for non boolean setting:", keyName, _value);
+  }
+
+  return (
+    <View style={[baseStyles.container, styles.switchContainer, (lessObviousStyling ? {} : baseStyles.whiteContainer)]}>
+      <View style={baseStyles.titleContainer}>
+        <Text style={baseStyles.titleText}>{title}</Text>
+        {description === undefined ? undefined : <Text style={baseStyles.descriptionText}>{description}</Text>}
+      </View>
+      {value === undefined ? undefined :
+        <Switch onValueChange={(newValue) => onPress?.(setValue, keyName, newValue)}
+                thumbColor={styles.switchComponent.color}
+                ios_backgroundColor={styles.switchComponent.backgroundColor}
+                value={_value} />}
+    </View>
+  );
+}
+
+
+export const createStyles = ({ isDark, colors }: ThemeContextProps) => StyleSheet.create({
+  switchContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  switchComponent: {
+    color: colors.switchComponentThumb,
+    backgroundColor: colors.switchComponentBackground
+  }
+});
+
+export default SettingSwitchComponent
