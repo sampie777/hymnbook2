3a5fc0b93e75ccd9581852b1e2022f4f3f28f229
S. Jansen <samuelanton@hotmail.com>
2023-06-05 11:03:28 +0200

    feat(songs): Added metadata to Song class.

--- __tests__/logic/songs/utils/createCopyrightTest.js
+++ __tests__/logic/songs/utils/createCopyrightTest.js
@@ -1,18 +1,20 @@
 import { createCopyright } from "../../../../source/logic/songs/utils";
-import { Song, SongBundle } from "../../../../source/logic/db/models/Songs";
+import { Song, SongBundle, SongMetadata, SongMetadataType } from "../../../../source/logic/db/models/Songs";
 
 describe("creating copyright for song", () => {
-  const song = new Song("", "", "", "", new Date(), new Date(), "", [], [], 0, 1);
+  const song = new Song("", "", new Date(), new Date(), "", [], [], [], 0, 1);
   const songBundle = new SongBundle("", "name", "", "", "", new Date(), new Date(), "000", "", [], 0);
   song._songBundles = [songBundle];
+
+  beforeEach(() => {
+    song.metadata = [];
+  });
 
   it("returns nothing if song is undefined", () => {
     expect(createCopyright(undefined)).toBe("");
   });
 
   it("returns the priority values as copyright", () => {
-    song.author = "";
-    song.copyright = "";
     song.language = "EN";
     songBundle.author = "";
     songBundle.copyright = "";
@@ -21,8 +23,6 @@ describe("creating copyright for song", () => {
   });
 
   it("includes the language if these are different as copyright", () => {
-    song.author = "";
-    song.copyright = "";
     song.language = "AF";
     songBundle.author = "";
     songBundle.copyright = "";
@@ -31,8 +31,6 @@ describe("creating copyright for song", () => {
   });
 
   it("includes the author if specified", () => {
-    song.author = "";
-    song.copyright = "";
     song.language = "EN";
     songBundle.author = "author";
     songBundle.copyright = "";
@@ -41,8 +39,7 @@ describe("creating copyright for song", () => {
   });
 
   it("includes the song author if specified", () => {
-    song.author = "songauthor";
-    song.copyright = "";
+    song.metadata.push(new SongMetadata(SongMetadataType.Author, "songauthor", 0));
     song.language = "EN";
     songBundle.author = "author";
     songBundle.copyright = "";
@@ -51,8 +48,7 @@ describe("creating copyright for song", () => {
   });
 
   it("includes the song author if specified but no bundle author is specified", () => {
-    song.author = "songauthor";
-    song.copyright = "";
+    song.metadata.push(new SongMetadata(SongMetadataType.Author, "songauthor", 0));
     song.language = "EN";
     songBundle.author = "";
     songBundle.copyright = "";
@@ -61,8 +57,6 @@ describe("creating copyright for song", () => {
   });
 
   it("includes the copyright if specified", () => {
-    song.author = "";
-    song.copyright = "";
     song.language = "EN";
     songBundle.author = "";
     songBundle.copyright = "copyright";
@@ -71,8 +65,7 @@ describe("creating copyright for song", () => {
   });
 
   it("includes the song copyright if specified", () => {
-    song.author = "";
-    song.copyright = "songcopyright";
+    song.metadata.push(new SongMetadata(SongMetadataType.Copyright, "songcopyright", 0));
     song.language = "EN";
     songBundle.author = "";
     songBundle.copyright = "copyright";
@@ -81,8 +74,7 @@ describe("creating copyright for song", () => {
   });
 
   it("includes the song copyright if specified but no bundle copyright is specified", () => {
-    song.author = "";
-    song.copyright = "songcopyright";
+    song.metadata.push(new SongMetadata(SongMetadataType.Copyright, "songcopyright", 0));
     song.language = "EN";
     songBundle.author = "";
     songBundle.copyright = "";
@@ -91,8 +83,6 @@ describe("creating copyright for song", () => {
   });
 
   it("includes all specified information", () => {
-    song.author = "";
-    song.copyright = "";
     song.language = "EN";
     songBundle.author = "author";
     songBundle.copyright = "copyright";
@@ -101,8 +91,8 @@ describe("creating copyright for song", () => {
   });
 
   it("includes all specified information if different", () => {
-    song.author = "songauthor";
-    song.copyright = "songcopyright";
+    song.metadata.push(new SongMetadata(SongMetadataType.Author, "songauthor", 0));
+    song.metadata.push(new SongMetadata(SongMetadataType.Copyright, "songcopyright", 0));
     song.language = "AF";
     songBundle.author = "author";
     songBundle.copyright = "copyright";
@@ -111,12 +101,15 @@ describe("creating copyright for song", () => {
   });
 
   it("includes nothing if no song bundle is specified", () => {
-    const song2 = new Song("", "", "", "", new Date(), new Date(), "", [], [], 0, 1);
+    const song2 = new Song("", "", new Date(), new Date(), "", [], [], [], 0, 1);
     expect(createCopyright(song2)).toBe("");
   });
 
   it("includes song specified information if no song bundle is specified", () => {
-    const song2 = new Song("", "songauthor", "songcopyright", "AF", new Date(), new Date(), "", [], [], 0, 1);
+    const song2 = new Song("", "AF", new Date(), new Date(), "", [], [], [
+      new SongMetadata(SongMetadataType.Author, "songauthor", 0),
+      new SongMetadata(SongMetadataType.Copyright, "songcopyright", 0),
+    ], 0, 1);
     expect(createCopyright(song2)).toBe("songauthor\nsongcopyright");
   });
 });
--- source/logic/server/models/ServerSongsModel.ts
+++ source/logic/server/models/ServerSongsModel.ts
@@ -1,3 +1,32 @@
+export enum SongMetadataType {
+  AlternativeTitle = "AlternativeTitle",
+  Copyright = "Copyright",
+  Author = "Author",
+  Year = "Year",
+  TextSource = "TextSource",
+  ScriptureReference = "ScriptureReference",
+  Superscription = "Superscription",
+}
+
+export class SongMetadata {
+  id: number;
+  type: SongMetadataType
+  value: string = ""
+  song: Song | null
+
+  constructor(
+    id: number,
+    type: SongMetadataType,
+    value: string = "",
+    song: Song | null,
+  ) {
+    this.id = id
+    this.type = type
+    this.value = value
+    this.song = song
+  }
+}
+
 export class AbcSubMelody {
   id: number;
   melody: string = "";
@@ -80,8 +109,6 @@ export class Song {
   id: number;
   name: string;
   number?: number;
-  author: string;
-  copyright: string;
   language: string;
   verses: Array<SongVerse> | null;
   songBundle: SongBundle | null;
@@ -89,12 +116,11 @@ export class Song {
   modifiedAt: string;
   uuid: string;
   abcMelodies?: AbcMelody[] | null;
+  metadata?: SongMetadata[] | null;
 
   constructor(
     id: number,
     name: string,
-    author: string,
-    copyright: string,
     language: string,
     verses: Array<SongVerse> | null,
     songBundle: SongBundle | null,
@@ -102,13 +128,12 @@ export class Song {
     modifiedAt: string,
     uuid: string,
     number?: number,
-    abcMelodies?: AbcMelody[] | null
+    abcMelodies?: AbcMelody[] | null,
+    metadata?: SongMetadata[] | null,
   ) {
     this.id = id;
     this.name = name;
     this.number = number;
-    this.author = author;
-    this.copyright = copyright;
     this.language = language;
     this.verses = verses;
     this.songBundle = songBundle;
@@ -116,6 +141,7 @@ export class Song {
     this.modifiedAt = modifiedAt;
     this.uuid = uuid;
     this.abcMelodies = abcMelodies;
+    this.metadata = metadata ?? [];
   }
 }
 
