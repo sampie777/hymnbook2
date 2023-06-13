49a4853c529ea77fdc100298e715952a492de5a7
S. Jansen <sa.jansen1@gmail.com>
2023-06-13 11:01:16 +0200

    feat(sourcemap): Moved secret to another file

--- upload_source_map.sh
+++ upload_source_map.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # See also: https://docs.rollbar.com/docs/react-native#source-maps
 
+# SERVERKEY should be defined in a file called upload_source_map_secrets.txt
 SERVERKEY=""
+
+source upload_source_map_secrets.txt
 VERSION=$(sed 's/.*"version": "\(.*\)".*/\1/;t;d' ./package.json)
 
 
