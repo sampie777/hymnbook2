b5cb16687b69c2661e62b4e6bcd609f905679498
S. Jansen <samuelanton@hotmail.com>
2023-09-11 12:22:24 +0200

    feat(ios): First setup for handling deep links in iOS. Still needs universal link URL.

--- ios/hymnbook2/AppDelegate.mm
+++ ios/hymnbook2/AppDelegate.mm
@@ -7,6 +7,7 @@
 
 #import <React/RCTAppSetupUtils.h>
 #import <RollbarReactNative/RollbarReactNative.h>
+#import <React/RCTLinkingManager.h>
 
 #if RCT_NEW_ARCH_ENABLED
 #import <React/CoreModulesPlugins.h>
@@ -132,5 +133,20 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 }
 
 #endif
+
+- (BOOL)application:(UIApplication *)application
+   openURL:(NSURL *)url
+   options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  return [RCTLinkingManager application:application openURL:url options:options];
+}
+
+- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity
+ restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
+{
+ return [RCTLinkingManager application:application
+                  continueUserActivity:userActivity
+                    restorationHandler:restorationHandler];
+}
 
 @end
