ac70242fa160d4d6eb8656e7b7994a8d1b0a8520
S. Jansen <samuelanton@hotmail.com>
2023-10-14 07:51:29 +0200

    feat(dependencies): Removed unused package

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
  * @format
  */
 
-import "react-native-get-random-values";
 import { AppRegistry } from "react-native";
 import { name as appName } from "./app.json";
 import { rollbar, rollbarInit } from "./source/logic/rollbar";
