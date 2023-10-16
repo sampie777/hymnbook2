6e0d73836deb5d10bbbdfda844d09b7b031546cb
S. Jansen <samuelanton@hotmail.com>
2023-10-16 13:12:24 +0200

    feat(audio): Stream play selected audio file

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 import { AppRegistry } from "react-native";
 import { name as appName } from "./app.json";
 import { rollbar, rollbarInit } from "./source/logic/rollbar";
+import { trackPlayerInit } from "./source/logic/songs/audiofiles/playerservice";
 
 AppRegistry.registerRunnable(appName, async initialProps => {
   try {
@@ -17,6 +18,9 @@ AppRegistry.registerRunnable(appName, async initialProps => {
     const App = require("./source/App").default;
     AppRegistry.registerComponent(appName, () => App);
     AppRegistry.runApplication(appName, initialProps);
+
+    const TrackPlayer = require("react-native-track-player").default;
+    TrackPlayer.registerPlaybackService(() => trackPlayerInit);
   } catch (e) {
     rollbar.error("Failed to run App", {
       error: e,
