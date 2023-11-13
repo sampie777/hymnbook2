9cf6b6f9d105a00559c0decf18f6ed10c8911ef5
S. Jansen <sa.jansen1@gmail.com>
2023-11-13 09:55:12 +0200

    feat(songsearch): Reload search results when previous number shadow clicked.

--- source/gui/screens/songs/search/SearchScreen.tsx
+++ source/gui/screens/songs/search/SearchScreen.tsx
@@ -165,6 +165,10 @@ const SearchScreen: React.FC<BottomTabScreenProps<ParamList, typeof SongSearchRo
                         showSongBundle={isTitleSimilarToOtherSongs(item, results)} />
     );
 
+    const onInputTextPress = () => {
+      setInputValue(previousInputValue);
+    }
+
     return (
       <View style={[styles.container, isPortraitMode(windowDimension) ? {} : stylesLandscape.container]}>
         <PopupsComponent navigation={navigation} />
@@ -187,7 +191,7 @@ const SearchScreen: React.FC<BottomTabScreenProps<ParamList, typeof SongSearchRo
 
               <View style={styles.inputTextView}>
                 <View style={styles.inputTextViewContainer}>
-                  <Text
+                  <Text onPress={onInputTextPress}
                     style={[styles.inputTextField, (!useSmallerFontSize ? {} : styles.inputTextFieldSmaller)]}>
                     {inputValue ? inputValue
                       : (!Settings.songSearchRememberPreviousEntry ? " " :
