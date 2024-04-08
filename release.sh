447778afaee6dc9dbfe91c140e7485ff7df381bb
S. Jansen <samuelanton@hotmail.com>
2024-04-08 10:30:47 +0200

    feat(release): Auto set JAVA_HOME to android studio if available

--- release.sh
+++ release.sh
@@ -2,6 +2,10 @@
 
 progname=$(basename $0)
 
+if [ -d ~/.local/share/JetBrains/Toolbox/apps/android-studio/jbr ]; then
+	export JAVA_HOME=~/.local/share/JetBrains/Toolbox/apps/android-studio/jbr
+fi
+
 function usage {
   cat << HEREDOC
 
