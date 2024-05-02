c6ce6256daff72f00fcec8e5a78c9828152849f5
S. Jansen <samuelanton@hotmail.com>
2024-05-02 12:43:45 +0200

    feat(songs): Store and display custom Abc melodies for verses.

---
+++ source/logic/songs/songUpdater.ts
@@ -0,0 +1,52 @@
+import { SongBundle as ServerSongBundle } from "../server/models/ServerSongsModel";
+import { sanitizeErrorForRollbar } from "../utils";
+import Db from "../db/db";
+import { rollbar } from "../rollbar";
+import { Server } from "../server/server";
+import { SongBundle } from "../db/models/Songs";
+import { SongUpdaterUtils } from "./songUpdaterUtils";
+
+export namespace SongUpdater {
+  export const fetchAndSaveSongBundle = (bundle: ServerSongBundle): Promise<any> =>
+    Server.fetchSongBundleWithSongsAndVerses(bundle)
+      .then(saveSongBundleToDatabase);
+
+  const saveSongBundleToDatabase = (bundle: ServerSongBundle) => {
+    SongUpdaterUtils.saveServerSongBundleToDatabase(bundle);
+  };
+
+  export const fetchAndUpdateSongBundle = (bundle: ServerSongBundle): Promise<any> =>
+    Server.fetchSongBundleWithSongsAndVerses(bundle)
+      .then(updateAndSaveSongBundle);
+
+  const updateAndSaveSongBundle = (bundle: ServerSongBundle) => {
+    SongUpdaterUtils.saveServerSongBundleToDatabase(bundle);
+  };
+
+  export const updateLocalBundlesWithUuid = (localBundles: SongBundle[], serverBundles: ServerSongBundle[]) => {
+    if (serverBundles.length === 0) {
+      return;
+    }
+
+    localBundles
+      .filter(it => it.uuid == "")
+      .forEach(it => {
+        const serverBundle = serverBundles.find(serverBundle => serverBundle.name == it.name);
+        if (serverBundle === undefined) {
+          return;
+        }
+
+        try {
+          Db.songs.realm().write(() => {
+            it.uuid = serverBundle.uuid;
+          });
+        } catch (error) {
+          rollbar.error(`Failed to update songbundle with new UUID`, {
+            ...sanitizeErrorForRollbar(error),
+            localBundle: { ...it, songs: null },
+            serverBundle: { ...serverBundle, songs: null }
+          });
+        }
+      });
+  };
+}
