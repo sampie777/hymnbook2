4d5047a2d062ad2b9ccc93d32589609c1874ed85
S. Jansen <sa.jansen1@gmail.com>
2024-06-24 13:41:56 +0200

    feat(pipeline): Added pipeline script

---
+++ ios/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+
+echo "===== Installing CocoaPods ====="
+export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
+brew install cocoapods
+
+echo "===== Installing Node.js ====="
+# installs nvm (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+# Load nvm and install Node.js
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install 18
+
+echo "===== Installing yarn ====="
+brew install yarn
+
+# Install dependencies
+echo "===== Running yarn install ====="
+yarn install
+
+echo "===== Running pod install ====="
+pwd
+pod install
