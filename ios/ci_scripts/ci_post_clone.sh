83b541c7fd458e87aed9be5bd46e02334fb49df3
S. Jansen <samuelanton@hotmail.com>
2024-07-29 08:05:57 +0200

    test environment variables in build pipeline

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -20,9 +20,14 @@ echo "===== Running yarn install ====="
 yarn install
 
 echo "===== Setting environment variables ====="
+echo "DEVELOPER_EMAIL=${DEVELOPER_EMAIL}"
+echo "ROLLBAR_API_KEY=${ROLLBAR_API_KEY}"
+ls -alh
 echo "ROLLBAR_API_KEY=${ROLLBAR_API_KEY}" > .env
 echo "DEVELOPER_EMAIL=${DEVELOPER_EMAIL}" >> .env
 echo "WHATSAPP_USER_GROUP_LINK=${WHATSAPP_USER_GROUP_LINK}" >> .env
+echo "== Reading back .env file =="
+cat .env
 
 echo "===== Running pod install ====="
 pwd
