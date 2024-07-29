d069d293037e5ed29e8f12bc8fb62dca99256d25
S. Jansen <samuelanton@hotmail.com>
2024-07-29 09:14:50 +0200

    write .env file to root directory

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 
 echo "===== Setting environment variables ====="
-echo "ROLLBAR_API_KEY=${ROLLBAR_API_KEY}" > .env
-echo "DEVELOPER_EMAIL=${DEVELOPER_EMAIL}" >> .env
-echo "WHATSAPP_USER_GROUP_LINK=${WHATSAPP_USER_GROUP_LINK}" >> .env
+echo "ROLLBAR_API_KEY=${ROLLBAR_API_KEY}" > ../../.env
+echo "DEVELOPER_EMAIL=${DEVELOPER_EMAIL}" >> ../../.env
+echo "WHATSAPP_USER_GROUP_LINK=${WHATSAPP_USER_GROUP_LINK}" >> ../../.env
 
 echo "===== Installing CocoaPods ====="
 export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
