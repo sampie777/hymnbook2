cd48d424214c09ec576b9f55b47fec23d5d0f797
S. Jansen <samuelanton@hotmail.com>
2024-09-11 09:53:23 +0200

    try to fetch any data from each scheme to see if that is possible regarding the app launch bug

--- source/logic/db/patches/utils.ts
+++ source/logic/db/patches/utils.ts
@@ -1,4 +1,6 @@
 import { DatabaseProvider } from "../dbProvider";
+import { rollbar } from "../../rollbar";
+import { sanitizeErrorForRollbar } from "../../utils";
 
 
 export const removeObjectsWithoutParents = (
@@ -10,22 +12,35 @@ export const removeObjectsWithoutParents = (
   }[],
 ) => {
   // Just so the compiler doesn't throw it all away, for debugging purposes.
-  console.log(db._isConnected, schemas.length);
-  // schemas.forEach(schema => {
-  //   const data = db.realm().objects(schema.schemaName)
-  //     .filtered(`${schema.parentLink}.@count = 0 ${schema.extraQuery ? schema.extraQuery : ""}`);
-  //
-  //   if (data.length == 0) return;
-  //
-  //   try {
-  //     db.realm().write(() => {
-  //       db.realm().delete(data);
-  //     });
-  //   } catch (error) {
-  //     rollbar.error(`Failed to delete unused ${schema.schemaName}`, {
-  //       ...sanitizeErrorForRollbar(error),
-  //       dataLength: data.length
-  //     });
-  //   }
-  // })
+  rollbar.debug("removeObjectsWithoutParents 0.0", {
+    dbIsConnected: db._isConnected,
+    schemasLength: schemas.length,
+    realmIsClosed: db.realm().isClosed,
+    realmIsInMigration: db.realm().isInMigration,
+    realmIsInTransaction: db.realm().isInTransaction,
+  });
+  schemas.forEach(schema => {
+    const data = db.realm().objects(schema.schemaName)
+    // .filtered(`${schema.parentLink}.@count = 0 ${schema.extraQuery ? schema.extraQuery : ""}`);
+    rollbar.debug("removeObjectsWithoutParents 0.1", {
+      dataLength: data.length,
+      isConnected: db._isConnected,
+      schemasLength: schemas.length,
+      realmIsClosed: db.realm().isClosed,
+      realmIsInMigration: db.realm().isInMigration,
+      realmIsInTransaction: db.realm().isInTransaction,
+    });
+    // if (data.length == 0) return;
+    //
+    // try {
+    //   db.realm().write(() => {
+    //     db.realm().delete(data);
+    //   });
+    // } catch (error) {
+    //   rollbar.error(`Failed to delete unused ${schema.schemaName}`, {
+    //     ...sanitizeErrorForRollbar(error),
+    //     dataLength: data.length
+    //   });
+    // }
+  })
 }
