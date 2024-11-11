99fc16c5e34f275ff71bde7db3244fed66fe3d19
S. Jansen <samuelanton@hotmail.com>
2024-11-11 09:36:01 +0200

    feat(db): added more error catching

--- source/logic/songs/songDbHelpers.ts
+++ source/logic/songs/songDbHelpers.ts
@@ -2,48 +2,132 @@ import { Song, SongBundle, SongMetadata, Verse } from "../db/models/Songs";
 import Db from "../db/db";
 import { SongListModel, SongListSongModel, SongListVerseModel } from "../db/models/SongListModel";
 import { AbcMelody, AbcSubMelody } from "../db/models/AbcMelodies";
+import { rollbar } from "../rollbar";
+import { sanitizeErrorForRollbar } from "../utils";
 
 export namespace SongDbHelpers {
   export const deleteAbcSubMelody = (obj: AbcSubMelody) => {
-    Db.songs.realm().delete(obj);
+    try {
+      Db.songs.realm().delete(obj);
+    } catch (error) {
+      rollbar.error("Failed to delete DB AbcSubMelody", {
+        ...sanitizeErrorForRollbar(error),
+        obj: { ...obj }
+      });
+      throw error;
+    }
   }
 
   export const deleteAbcMelody = (obj: AbcMelody) => {
     for (const child of obj.subMelodies) deleteAbcSubMelody(child);
-    Db.songs.realm().delete(obj);
+    try {
+      Db.songs.realm().delete(obj);
+    } catch (error) {
+      rollbar.error("Failed to delete DB AbcMelody", {
+        ...sanitizeErrorForRollbar(error),
+        obj: { ...obj, subMelodies: null, }
+      });
+      throw error;
+    }
   }
 
   export const deleteVerse = (obj: Verse) => {
-    Db.songs.realm().delete(obj);
+    try {
+      Db.songs.realm().delete(obj);
+    } catch (error) {
+      rollbar.error("Failed to delete DB Verse", {
+        ...sanitizeErrorForRollbar(error),
+        obj: { ...obj }
+      });
+      throw error;
+    }
   }
 
   export const deleteSongMetadata = (obj: SongMetadata) => {
-    Db.songs.realm().delete(obj);
+    try {
+      Db.songs.realm().delete(obj);
+    } catch (error) {
+      rollbar.error("Failed to delete DB SongMetadata", {
+        ...sanitizeErrorForRollbar(error),
+        obj: { ...obj }
+      });
+      throw error;
+    }
   }
 
   export const deleteSong = (obj: Song) => {
     for (const child of obj.verses) deleteVerse(child);
     for (const child of obj.metadata) deleteSongMetadata(child);
     for (const child of obj.abcMelodies) deleteAbcMelody(child);
-    Db.songs.realm().delete(obj);
+    try {
+      Db.songs.realm().delete(obj);
+    } catch (error) {
+      rollbar.error("Failed to delete DB Song", {
+        ...sanitizeErrorForRollbar(error),
+        obj: {
+          ...obj,
+          verses: null,
+          abcMelodies: null,
+          metadata: null,
+          _songBundles: null,
+        }
+      });
+      throw error;
+    }
   }
 
   export const deleteSongBundle = (obj: SongBundle) => {
     for (const child of obj.songs) deleteSong(child);
-    Db.songs.realm().delete(obj);
+    try {
+      Db.songs.realm().delete(obj);
+    } catch (error) {
+      rollbar.error("Failed to delete DB SongBundle", {
+        ...sanitizeErrorForRollbar(error),
+        obj: { ...obj, songs: null, }
+      });
+      throw error;
+    }
   }
 
   export const deleteSongListVerse = (obj: SongListVerseModel) => {
-    Db.songs.realm().delete(obj);
+    try {
+      Db.songs.realm().delete(obj);
+    } catch (error) {
+      rollbar.error("Failed to delete DB SongListVerseModel", {
+        ...sanitizeErrorForRollbar(error),
+        obj: { ...obj }
+      });
+      throw error;
+    }
   }
 
   export const deleteSongListSong = (obj: SongListSongModel) => {
     for (const child of obj.selectedVerses) deleteSongListVerse(child);
-    Db.songs.realm().delete(obj);
+    try {
+      Db.songs.realm().delete(obj);
+    } catch (error) {
+      rollbar.error("Failed to delete DB SongListSongModel", {
+        ...sanitizeErrorForRollbar(error),
+        obj: {
+          ...obj,
+          song: null,
+          selectedVerses: null,
+        }
+      });
+      throw error;
+    }
   }
 
   export const deleteSongList = (obj: SongListModel) => {
     for (const child of obj.songs) deleteSongListSong(child);
-    Db.songs.realm().delete(obj);
+    try {
+      Db.songs.realm().delete(obj);
+    } catch (error) {
+      rollbar.error("Failed to delete DB SongListModel", {
+        ...sanitizeErrorForRollbar(error),
+        obj: { ...obj, songs: null, }
+      });
+      throw error;
+    }
   }
 }
