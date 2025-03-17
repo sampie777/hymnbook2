12cb652e73a4898837ed7d5d5ed34eddde0342ff
S. Jansen <samuelanton@hotmail.com>
2025-03-17 13:12:07 +0200

    fix(audio): Don't launch Hymnbook when no audio is available but Play button is pressed.

--- source/logic/songs/audiofiles/playerservice.ts
+++ source/logic/songs/audiofiles/playerservice.ts
@@ -2,7 +2,15 @@ import TrackPlayer, { Event } from "react-native-track-player";
 
 export const trackPlayerInit = async () => {
   try {
-    TrackPlayer.addEventListener(Event.RemotePlay, () => TrackPlayer.play());
+    TrackPlayer.addEventListener(Event.RemotePlay, async () => {
+      // Don't fire up the player when there's nothing to play
+      const queue = await TrackPlayer.getQueue();
+      if (queue.length == 0) {
+        await TrackPlayer.reset()
+        return;
+      }
+      TrackPlayer.play()
+    });
     TrackPlayer.addEventListener(Event.RemotePause, () => TrackPlayer.pause());
     TrackPlayer.addEventListener(Event.RemoteStop, () => TrackPlayer.stop());
     TrackPlayer.addEventListener(Event.RemoteJumpBackward, () => TrackPlayer.seekTo(0));
