8f3023bdf8128ab9621497ef89d7ef1c95c3c9b3
S. Jansen <samuelanton@hotmail.com>
2025-06-02 15:08:13 +0200

    try something with the post_clone.sh file

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -8,6 +8,7 @@ echo "WHATSAPP_USER_GROUP_LINK=${WHATSAPP_USER_GROUP_LINK}" >> ../../.env
 echo "===== Installing CocoaPods ====="
 export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
 brew install cocoapods
+echo "Installed version $(pod --version)}"
 
 echo "===== Installing Node.js ====="
 # installs nvm (Node Version Manager)
@@ -24,13 +25,21 @@ brew install yarn
 echo "===== Running yarn install ====="
 yarn install
 
+echo "===== Running pd repo update ====="
+pod repo update
 echo "===== Running pod install ====="
-function podInstall {
-  pod install && return
-  echo "  === Running pod update ===  "
-  pod update
-  echo "  === Running pod install ===  "
-  pod install && return
-  exit 1
-}
-podInstall
+pod install
+echo "===== Running pod update ====="
+pod update
+
+
+#echo "===== Running pod install ====="
+#function podInstall {
+#  pod install && return
+#  echo "  === Running pod update ===  "
+#  pod update
+#  echo "  === Running pod install ===  "
+#  pod install && return
+#  exit 1
+#}
+#podInstall
