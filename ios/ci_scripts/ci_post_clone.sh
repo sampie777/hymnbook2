6924a710e45e6298b1b164eeb589514e3a4d9b5c
S. Jansen <samuelanton@hotmail.com>
2025-06-07 20:09:27 +0200

    feat(dev): Reverted Xcoude Clode install script

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -8,7 +8,6 @@ echo "WHATSAPP_USER_GROUP_LINK=${WHATSAPP_USER_GROUP_LINK}" >> ../../.env
 echo "===== Installing CocoaPods ====="
 export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
 brew install cocoapods
-echo "Installed version $(pod --version)}"
 
 echo "===== Installing Node.js ====="
 # installs nvm (Node Version Manager)
@@ -25,21 +24,13 @@ brew install yarn
 echo "===== Running yarn install ====="
 yarn install
 
-#echo "===== Running pd repo update ====="
-#pod repo update
 echo "===== Running pod install ====="
-USE_FRAMEWORKS=static pod install
-echo "===== Running pod update ====="
-USE_FRAMEWORKS=static pod update
-
-
-#echo "===== Running pod install ====="
-#function podInstall {
-#  pod install && return
-#  echo "  === Running pod update ===  "
-#  pod update
-#  echo "  === Running pod install ===  "
-#  pod install && return
-#  exit 1
-#}
-#podInstall
+function podInstall {
+  pod install && return
+  echo "  === Running pod update ===  "
+  pod update
+  echo "  === Running pod install ===  "
+  pod install && return
+  exit 1
+}
+podInstall
