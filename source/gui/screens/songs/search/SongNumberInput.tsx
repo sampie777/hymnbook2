50a0a30c94a7325460e44f9dcf01c4851fe5f7ca
S. Jansen <samuelanton@hotmail.com>
2025-08-20 13:58:14 +0200

    fix(search): Don't allow a physical keyboard on Android, as it pop ups the onscreen keyboard.

--- source/gui/screens/songs/search/SongNumberInput.tsx
+++ source/gui/screens/songs/search/SongNumberInput.tsx
@@ -1,13 +1,14 @@
 import React, { Dispatch, SetStateAction } from 'react';
 import {
   StyleSheet,
-  TextInput,
   View,
 } from 'react-native';
-import Settings from "../../../../settings";
 import { isIOS } from "../../../../logic/utils";
 import { ThemeContextProps, useTheme } from "../../../components/providers/ThemeProvider";
-import config from '../../../../config';
+import {
+  SongNumberInputTextAndroid,
+  SongNumberInputTextIOS,
+} from './SongNumberInputText.tsx';
 
 type Props = {
   value: string
@@ -20,41 +21,18 @@ type Props = {
 const SongNumberInput: React.FC<Props> = ({ value, previousValue, onPress, useSmallerFontSize, setInputValue }) => {
   const styles = createStyles(useTheme());
 
-  // Use `previousValue`, so we don't have to use the `inputValue` state directly,
-  // which causes unnecessary component updates.
-  const onNumberKeyPress = (number: number) =>
-    setInputValue((prev) =>
-       prev.length >= config.maxSearchInputLength ? prev : prev + number
-    );
-
-  const onDeleteKeyPress = () =>
-    setInputValue(prev => prev.length <= 1 ? "" : prev.substring(0, prev.length - 1))
-
-  const onKeyPress = (key: string) => {
-    if (key === "Backspace") {
-      onDeleteKeyPress()
-    } else if (!isNaN(+key)) {
-      onNumberKeyPress(+key)
-    }
-  }
+  const TextLabelElement = isIOS ? SongNumberInputTextIOS : SongNumberInputTextAndroid;
 
   return (
     <View style={styles.container}>
       <View style={styles.innerContainer}>
-        <TextInput
-          caretHidden={true}
-          onPressIn={Settings.songSearchRememberPreviousEntry || value ? undefined : onPress}
-          onKeyPress={e => onKeyPress(e.nativeEvent.key)}
+        <TextLabelElement
           value={value}
-          placeholder={Settings.songSearchRememberPreviousEntry ? previousValue : undefined}
-          placeholderTextColor={styles.placeholder.color}
-          style={[
-            styles.text,
-            !useSmallerFontSize ? {} : styles.textSmaller,
-            Settings.songSearchRememberPreviousEntry && !value ? styles.placeholder : {}
-          ]}
-          importantForAccessibility={value ? 'auto' : 'no'}
-          accessibilityElementsHidden={!value}></TextInput>
+          previousValue={previousValue}
+          useSmallerFontSize={useSmallerFontSize}
+          onPress={onPress}
+          setInputValue={setInputValue}
+          />
       </View>
     </View>
   );
@@ -62,7 +40,7 @@ const SongNumberInput: React.FC<Props> = ({ value, previousValue, onPress, useSm
 
 export default SongNumberInput;
 
-const createStyles = ({ isDark, colors, fontFamily }: ThemeContextProps) => StyleSheet.create({
+const createStyles = ({ isDark }: ThemeContextProps) => StyleSheet.create({
   container: {
     flexDirection: "row",
     alignItems: "center"
@@ -73,19 +51,5 @@ const createStyles = ({ isDark, colors, fontFamily }: ThemeContextProps) => Styl
     borderBottomWidth: 2,
     borderBottomColor: isDark ? "#404040" : "#ddd",
     minWidth: 140
-  },
-  text: {
-    fontSize: 70,
-    textAlign: "center",
-    fontFamily: fontFamily.sansSerifLight,
-    color: colors.text.light,
-  },
-  placeholder: {
-    color: isDark ? (isIOS ? "#303030" : "#2a2a2a00") : "#e5e5e5",
-    textShadowColor: isDark ? (isIOS ? "#404040" : "#404040aa") : "#ddd",
-    textShadowRadius: 12
-  },
-  textSmaller: {
-    fontSize: 40
   },
 });
---
+++ source/gui/screens/songs/search/SongNumberInputText.tsx
@@ -0,0 +1,88 @@
+import React, {Dispatch, SetStateAction} from 'react';
+import {StyleSheet, Text, TextInput} from 'react-native';
+import Settings from '../../../../settings.ts';
+import {isIOS} from '../../../../logic/utils.ts';
+import {
+  ThemeContextProps,
+  useTheme,
+} from '../../../components/providers/ThemeProvider.tsx';
+import config from '../../../../config.ts';
+
+interface Props {
+  value: string
+  previousValue: string
+  useSmallerFontSize: boolean
+  onPress?: () => void
+}
+
+export const SongNumberInputTextAndroid: React.FC<Props> = ({value, previousValue, onPress, useSmallerFontSize}) => {
+  const styles = createStyles(useTheme());
+
+  return <Text onPress={onPress}
+               style={[styles.text, (!useSmallerFontSize ? {} : styles.textSmaller)]}
+               importantForAccessibility={value ? "auto" : "no"}
+               accessibilityElementsHidden={!value}>
+    {value ? value
+      : (!Settings.songSearchRememberPreviousEntry ? " " :
+          <>{isIOS ? "" : " "}<Text
+            style={styles.placeholder}>{previousValue}</Text> </>
+      )
+    }
+  </Text>
+};
+
+export const SongNumberInputTextIOS: React.FC<Props & { setInputValue: Dispatch<SetStateAction<string>> }> = ({value, previousValue, onPress, useSmallerFontSize, setInputValue}) => {
+  const styles = createStyles(useTheme());
+
+  // Use `previousValue`, so we don't have to use the `inputValue` state directly,
+  // which causes unnecessary component updates.
+  const onNumberKeyPress = (number: number) =>
+    setInputValue((prev) =>
+      prev.length >= config.maxSearchInputLength ? prev : prev + number
+    );
+
+  const onDeleteKeyPress = () =>
+    setInputValue(prev => prev.length <= 1 ? "" : prev.substring(0, prev.length - 1))
+
+  const onKeyPress = (key: string) => {
+    if (key === "Backspace") {
+      onDeleteKeyPress()
+    } else if (!isNaN(+key)) {
+      onNumberKeyPress(+key)
+    }
+  }
+
+  return <TextInput
+    caretHidden={true}
+    keyboardType={'decimal-pad'}
+    inputMode={'decimal'}
+    onPressIn={!Settings.songSearchRememberPreviousEntry || value ? () => console.debug("Nothing", Settings.songSearchRememberPreviousEntry, value) : onPress}
+    onKeyPress={e => onKeyPress(e.nativeEvent.key)}
+    value={value}
+    placeholder={Settings.songSearchRememberPreviousEntry ? previousValue : undefined}
+    placeholderTextColor={styles.placeholder.color}
+    style={[
+      styles.text,
+      !useSmallerFontSize ? {} : styles.textSmaller,
+      Settings.songSearchRememberPreviousEntry && !value ? styles.placeholder : {}
+    ]}
+    importantForAccessibility={value ? 'auto' : 'no'}
+    accessibilityElementsHidden={!value}></TextInput>
+};
+
+const createStyles = ({ isDark, colors, fontFamily }: ThemeContextProps) => StyleSheet.create({
+  text: {
+    fontSize: 70,
+    textAlign: "center",
+    fontFamily: fontFamily.sansSerifLight,
+    color: colors.text.light,
+  },
+  placeholder: {
+    color: isDark ? (isIOS ? "#303030" : "#2a2a2a00") : "#e5e5e5",
+    textShadowColor: isDark ? (isIOS ? "#404040" : "#404040aa") : "#ddd",
+    textShadowRadius: 12
+  },
+  textSmaller: {
+    fontSize: 40
+  },
+});
