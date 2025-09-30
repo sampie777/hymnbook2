2c14261d972fadd89946e6797d96ba9a25cad3b9
S. Jansen <samuelanton@hotmail.com>
2025-09-30 21:20:12 +0200

    WIP: other files

--- index.js
+++ index.js
@@ -2,13 +2,12 @@
  * @format
  */
 
-import "react-native-gesture-handler";  // First import to prevent crashes in iOS (https://docs.swmansion.com/react-native-gesture-handler/docs/fundamentals/installation#ios)
-import { AppRegistry } from "react-native";
-import { sanitizeErrorForRollbar } from "./source/logic/utils";
-import { rollbar, rollbarInit } from "./source/logic/rollbar";
-import { trackPlayerInit } from "./source/logic/songs/audiofiles/playerservice";
+import {AppRegistry} from "react-native";
+import {sanitizeErrorForRollbar} from "./source/logic/utils";
+import {rollbar, rollbarInit} from "./source/logic/rollbar";
+import {trackPlayerInit} from "./source/logic/songs/audiofiles/playerservice";
 import App from "./source/App";
-import { name as appName } from "./app.json";
+import {name as appName} from "./app.json";
 
 try {
   rollbarInit();
