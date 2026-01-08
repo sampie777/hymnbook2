9ef7b4c87f8d3e6f3c330ded2d305559bf2e63df
S. Jansen <samuelanton@hotmail.com>
2026-01-08 18:59:51 +0200

    refactor(donations): Improved code and display

--- source/gui/components/donations/InAppPurchases.tsx
+++ source/gui/components/donations/InAppPurchases.tsx
@@ -1,77 +1,75 @@
 import React, { useEffect } from "react";
-import { StyleSheet, View } from "react-native";
+import { Alert, StyleSheet, View } from "react-native";
 import { PurchaseIOS, useIAP, VerifyPurchaseResultIOS } from "react-native-iap";
-import { isIOS } from "../../../logic/utils/utils.ts";
-import { ThemeContextProps, useTheme } from "../providers/ThemeProvider.tsx";
+import { isIOS, sanitizeErrorForRollbar } from "../../../logic/utils/utils.ts";
 import InAppPurchaseItem from "./InAppPurchaseItem.tsx";
+import { rollbar } from "../../../logic/rollbar.ts";
+import { Donations } from "../../../logic/donations.ts";
 
 interface Props {
 }
 
-const productIds = ['nl.sajansen.hymnbook2.donation100'];
-
 const InAppPurchases: React.FC<Props> = ({}) => {
   if (!isIOS) return null;
 
-  const styles = createStyles(useTheme())
-
   const { connected, products, fetchProducts, requestPurchase, verifyPurchase, } = useIAP({
     onPurchaseSuccess: (purchase) => {
-      console.log('Purchase successful:', purchase);
-      // Handle successful purchase
       validatePurchase(purchase as PurchaseIOS);
     },
     onPurchaseError: (error) => {
-      console.error('Purchase failed:', error);
-      // Handle purchase error
+      rollbar.error('Failed to complete in-app purchase', sanitizeErrorForRollbar(error));
     },
   });
 
   useEffect(() => {
     if (connected) {
-      fetchProducts({ skus: productIds, type: 'in-app' });
+      fetchProducts({ skus: Donations.inAppProductIds, type: 'in-app' });
     }
   }, [connected]);
 
   const validatePurchase = async (purchase: PurchaseIOS) => {
     try {
-      const result = (await verifyPurchase({
-        apple: { sku: purchase.productId },
-      })) as VerifyPurchaseResultIOS;
+      const result = (await verifyPurchase({ apple: { sku: purchase.productId } })) as VerifyPurchaseResultIOS;
 
-      if (result.isValid) {
-        // Grant user the purchased content
-        console.log('Receipt is valid');
-      } else {
-        // This should never be reached as Apple already validates the receipt and will throw an error if invalid
-        console.error('Receipt is invalid');
+      if (!result.isValid) {
+        rollbar.warning("In-app purchase receipt is invalid", { purchase, result });
+        return;
       }
+
+      Alert.alert("Thank you!", "Your donation is greatly appreciated\nand motivates me to keep on going with this work.");
     } catch (error) {
-      console.error('Validation failed:', error);
+      rollbar.warning("Failed to validate in-app purchase", { ...sanitizeErrorForRollbar(error), purchase });
     }
   };
 
   return (
     <View style={styles.container}>
-      {products.map(product => (
-          <InAppPurchaseItem
-            key={product.id}
-            product={product}
-            onPress={() => requestPurchase({
+      {products.map(product =>
+        <InAppPurchaseItem
+          key={product.id}
+          product={product}
+          onPress={() => {
+            rollbar.info("Requesting in-app purchase", { productId: product.id });
+            requestPurchase({
               request: { apple: { sku: product.id } },
               type: 'in-app',
-            })} />
-        )
+            })
+          }} />
       )}
     </View>
   );
 };
 
-const createStyles = ({ colors, fontFamily }: ThemeContextProps) =>
-  StyleSheet.create({
-    container: {
-      marginBottom: 20,
-    },
-  });
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    paddingHorizontal: 30,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    gap: 20,
+  },
+});
 
 export default InAppPurchases;
